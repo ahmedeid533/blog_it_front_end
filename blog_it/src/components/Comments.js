be8eeaee8f5c44ed896acc09cb42b48b0947015d
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-06-02 10:51:25 +0300

    avatar

--- blog_it/src/components/Comments.js
+++ blog_it/src/components/Comments.js
@@ -70,7 +70,7 @@ function Comments(probs) {
 							<ul>
 								<li key={index}>
 								<div className="author-thumb">
-									<img src={process.env.PUBLIC_URL + `/assets/images/comment-author-0${index%2 + 1}.jpg`} alt="fake image" />
+									<img src={process.env.PUBLIC_URL + `/assets/images/Profile_Avatar.png`} alt="fake image" />
 								</div>
 								<div className="right-content">
 									<h4>{comment.username ? comment.username : "private"}<span>{comment.createdAt.split("T")[0]}</span></h4>
